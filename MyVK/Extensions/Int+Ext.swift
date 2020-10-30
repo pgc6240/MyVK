045c4c8e37cad95534f07d0049c7038b6aff1a2d
pgc6240 <pgc6240@gmail.com>
2020-10-31 00:13:33 +0300

    added compositional layout to PhotosVC

---
+++ MyVK/Extensions/Int+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Int+Ext.swift
+//  MyVK
+//
+//  Created by pgc6240 on 31.10.2020.
+//
+
+extension Int {
+    
+    var isEven: Bool {
+        self % 2 == 0
+    }
+}
--- MyVK/Screens/PhotosVC.swift
+++ MyVK/Screens/PhotosVC.swift
@@ -9,17 +9,41 @@ import UIKit
 
 class PhotosVC: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        collectionView.collectionViewLayout = collectionViewLayout
+    }
+    
+    var collectionViewLayout: UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            let columns = sectionIndex.isEven ? 3 : 2
+            let spacing: CGFloat = 8
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(150))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+            group.interItemSpacing = NSCollectionLayoutSpacing.fixed(spacing)
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = spacing
+            section.contentInsets = NSDirectionalEdgeInsets(top: spacing, leading: spacing, bottom: 0, trailing: spacing)
+            return section
+        }
+        return layout
     }
 }
 
 
 extension PhotosVC: UICollectionViewDataSource, UICollectionViewDelegate {
 
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        10
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        Int.random(in: 1..<100)
+        6
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
