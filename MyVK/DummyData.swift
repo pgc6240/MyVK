aacdae0facdb8c0bb90a9255f0fcab0b21390e8b
pgc6240 <pgc6240@gmail.com>
2020-10-30 21:42:46 +0300

    quick bugfix

---
+++ MyVK/DummyData.swift
@@ -0,0 +1,14 @@
+//
+//  DummyData.swift
+//  MyVK
+//
+//  Created by pgc6240 on 30.10.2020.
+//
+
+let dummyGroups: [Group] = {
+    var groups: [Group] = []
+    for i in 0..<Int.random(in: 1...100) {
+        groups.append(Group(name: "Сообщество \(i)"))
+    }
+    return groups
+}()
