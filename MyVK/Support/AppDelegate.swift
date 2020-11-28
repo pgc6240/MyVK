4c111f40eddcd6928dcddace3858951d2600c330
pgc6240 <pgc6240@gmail.com>
2020-11-28 20:58:50 +0300

    24

--- MyVK/Support/AppDelegate.swift
+++ MyVK/Support/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UserDefaults.standard.set("ru", forKey: "AppleLanguages")
+        UserDefaults.standard.set(["ru"], forKey: "AppleLanguages")
         return true
     }
     
