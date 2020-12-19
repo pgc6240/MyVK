b2f45fed4b2001910f25d58aa8800aecfdd20aa3
pgc6240 <pgc6240@gmail.com>
2020-12-19 17:56:21 +0300

    добавил Alamofire в проект

---
+++ MyVK/Managers/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  MyVK
+//
+//  Created by pgc6240 on 19.12.2020.
+//
+
+import Alamofire
+
+final class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+}
