43bc80145a7bb8fa718454d215d550d9e621949a
pgc6240 <pgc6240@gmail.com>
2020-12-22 21:51:44 +0300

    a bit of refactoring

--- MyVK/Extensions/Swift+Ext.swift
+++ MyVK/Extensions/Swift+Ext.swift
@@ -9,11 +9,8 @@ import Foundation
 
 extension Int {
     
-    var isEven: Bool { self % 2 == 0 }
-    
-    
-    init?(_ string: String?) {
-        guard let string = string else { return nil }
+    init?(_ optionalString: String?) {
+        guard let string = optionalString else { return nil }
         self.init(string)
     }
 }
@@ -21,45 +18,8 @@ extension Int {
 
 extension String {
     
-    var isValidPhoneNumber: Bool {
-        let phoneNumberFormat    = "^((\\+7|7|8)+([0-9]){10})$"
-        let phoneNumberPredicate = NSPredicate(format: "SELF MATCHES %@", phoneNumberFormat)
-        return phoneNumberPredicate.evaluate(with: self)
-    }
-    
-    var isValidEmail: Bool {
-        let emailFormat    = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
-        return emailPredicate.evaluate(with: self)
-    }
-    
-    var isValidPassword: Bool {
-        let passwordFormat    = ".{8,}"
-        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordFormat)
-        return passwordPredicate.evaluate(with: self)
-    }
-    
-    var toLatin: String {
-        let latinString = self.applyingTransform(StringTransform.toLatin, reverse: false) ?? self
-        return latinString.applyingTransform(StringTransform.stripDiacritics, reverse: false) ?? self
-    }
-    
     var localized: String {
         NSLocalizedString(self, comment: "")
-    }
-    
-    var isProbablyEmail: Bool {
-        self.contains("@")
-    }
-    
-    mutating func removeCharacters(_ character: Character...) {
-        character.forEach { characterToRemove in self = self.filter { $0 != characterToRemove }}
-    }
-    
-    
-    init?(_ int: Int?) {
-        guard let int = int else { return nil }
-        self.init(int)
     }
 }
 
