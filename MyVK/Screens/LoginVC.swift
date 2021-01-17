c24edcc2e85db8a3f0a32d97e47921c95b21431c
pgc6240 <pgc6240@gmail.com>
2021-01-18 00:48:57 +0300

    bug fixes

--- MyVK/Screens/LoginVC.swift
+++ MyVK/Screens/LoginVC.swift
@@ -12,9 +12,6 @@ final class LoginVC: UIViewController {
     
     private var webView: WKWebView!
     
-    @UserDefault(key: "appId", defaultValue: C.appIds.first)
-    private var appId: String?
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +32,7 @@ final class LoginVC: UIViewController {
     private func loadAuthPage() {
         var urlComponents = URLComponents(string: "https://oauth.vk.com/authorize")
         let parameters    = [
-            "client_id"     : appId,
+            "client_id"     : C.APP_IDS[3],
             "redirect_uri"  : "https://oauth.vk.com/blank.html",
             "display"       : "mobile",
             "scope"         : "262150",
@@ -61,9 +58,7 @@ extension LoginVC: WKNavigationDelegate {
     func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
         
         guard let url = navigationResponse.response.url, url.path == "/blank.html", let fragment = url.fragment else {
-            decisionHandler(.cancel)
-            appId = C.appIds.randomElement()
-            loadAuthPage()
+            decisionHandler(.allow)
             return
         }
         
