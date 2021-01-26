7dd9ef643031b78224b47de9eb8f4873fc4447e4
pgc6240 <pgc6240@gmail.com>
2021-01-26 20:40:46 +0300

    Change spring in zoom animation

--- MyVK/Views/ImageView/MyImageView.swift
+++ MyVK/Views/ImageView/MyImageView.swift
@@ -9,25 +9,29 @@ import UIKit
 
 final class MyImageView: UIImageView {
     
+    // MARK: - Storyboard-editable propeties
     @IBInspectable var cornerRadius: CGFloat = 0 {
         willSet { layer.cornerRadius = newValue }
     }
     
     
-    // MARK: - Initialization -
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(zoom)))
+    // MARK: - Spring in zoom animation -
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 100, initialSpringVelocity: 5, options: [.allowUserInteraction]) {
+            self.transform = CGAffineTransform(scaleX: 2, y: 2)
+        }
     }
     
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 100, initialSpringVelocity: 5, options: [.allowUserInteraction]) {
+            self.transform = .identity
+        }
+    }
     
-    // MARK: - Spring in zoom animation -
-    @objc private func zoom() {
-        let zoomAnimation = CASpringAnimation(keyPath: "transform.scale")
-        zoomAnimation.toValue = 2
-        zoomAnimation.autoreverses = true
-        
-        layer.add(zoomAnimation, forKey: nil)
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 100, initialSpringVelocity: 5, options: [.allowUserInteraction]) {
+            self.transform = .identity
+        }
     }
     
     
